efe18268809aa57e992046594946a811e82de9f1
ousemaakacha <akachaousema@gmail.com>
2025-11-13 15:01:53 +0100

    feat: initial form structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,19 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div>
+        <h1>REACT POST FORM</h1>
+        <form>
+          <div>
+            <label></label>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
